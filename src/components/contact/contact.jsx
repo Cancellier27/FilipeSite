569e1b07f08a04c8ae8849c2d917f9f995527b19
Cancellier27 <filipecancelliercosta@gmail.com>
2020-06-10 21:01:25 +0100

    Created Contact layout

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 import './contact.css'
 
 const Contact = () => (
-  <h1>oi</h1>
+  <main className="ContactContainer">
+    <div className="formContainer">
+      <form action="submit" className="formItSelf">
+        <div className="divName">
+        <input type="text" placeholder="Name" />
+        </div>
+        <div className="divEmailPhone">
+        <input type="text" placeholder="E-mail" />
+        <input type="text" placeholder="Phone" className="phoneInput" />  
+        </div>
+        <div>
+          <textarea type="text" placeholder="Contact Subject" maxlength="400" />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+
+    <div className="contactText">
+      <p className="pTextContact">
+        Hello, hope you have enjoyed my work
+        <br /><br />
+        If you have any questions about me, you can send me a message on the left,
+        <br />
+        and I'll try to answer you as soon as posible.
+        <br /><br />
+        Thank you, and have a good day!
+      </p>
+    </div>
+  </main>
 )
 
 export default Contact
