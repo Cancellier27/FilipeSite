3fd21c2f8d5576f4f7ee06913975970770a6a2ca
Cancellier27 <filipecancelliercosta@gmail.com>
2020-06-11 18:26:15 +0100

    Updates

---
+++ precache-manifest.01fe69c48fe6c79c50f96d58bd641ccf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c10d22e5d8baabbad690f9ca150130b",
+    "url": "/FilipeSite/index.html"
+  },
+  {
+    "revision": "1c884807a6fce4ad5945",
+    "url": "/FilipeSite/static/css/main.f0dbc7cb.chunk.css"
+  },
+  {
+    "revision": "a467164b8d725e28cb76",
+    "url": "/FilipeSite/static/js/2.7843c423.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FilipeSite/static/js/2.7843c423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c884807a6fce4ad5945",
+    "url": "/FilipeSite/static/js/main.e6005168.chunk.js"
+  },
+  {
+    "revision": "c2a1d58d24e30ebc6cdf",
+    "url": "/FilipeSite/static/js/runtime-main.a87e3311.js"
+  },
+  {
+    "revision": "b28ec7b18871d6aaa00e8b4de34d53c1",
+    "url": "/FilipeSite/static/media/DoomFire.b28ec7b1.PNG"
+  },
+  {
+    "revision": "1ced5318cae2d6f06028f0a03ef4389b",
+    "url": "/FilipeSite/static/media/EcoletaSite.1ced5318.PNG"
+  },
+  {
+    "revision": "a23d87817751e5f3595bf13e6ab717ef",
+    "url": "/FilipeSite/static/media/FilipeCancellier.a23d8781.png"
+  },
+  {
+    "revision": "fe23c66acfb20da2e2bb9360b2530754",
+    "url": "/FilipeSite/static/media/Forca.fe23c66a.PNG"
+  },
+  {
+    "revision": "13306c3dc01fed67ca4bc6a61c86c70f",
+    "url": "/FilipeSite/static/media/PersonalSite.13306c3d.png"
+  },
+  {
+    "revision": "97ca7a226e521a4db4613df602b3d2b3",
+    "url": "/FilipeSite/static/media/helloWorld.97ca7a22.png"
+  },
+  {
+    "revision": "0471dcd811054cdd41bb78096cd251f4",
+    "url": "/FilipeSite/static/media/jogoDaVelha.0471dcd8.PNG"
+  },
+  {
+    "revision": "156e0b25e139c036982e97426bcbbc3f",
+    "url": "/FilipeSite/static/media/timerReact.156e0b25.PNG"
+  }
+]);
